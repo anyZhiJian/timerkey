8859fc45e94b92eed4807be6342871a99405d944
陟渐 <937279457@qq.com>
2025-05-14 14:34:11 +0800

    first commit

---
+++ timerkey.h
@@ -0,0 +1,55 @@
+#ifndef __TIMERKEY_H
+#define __TIMERKEY_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define tkey_malloc(size)   malloc(size)
+#define tkey_free(ptr)      free(ptr)
+
+typedef enum
+{
+    TKEY_EVENT_PRESS,
+    TKEY_EVENT_LONG_PRESS,
+    TKEY_EVENT_RELEASE,
+    TKEY_EVENT_LONG_RELEASE,
+    TKEY_EVENT_MULTI_PRESS,
+    TKEY_EVENT_MULTI_RELEASE,
+} tkey_event_t;
+
+typedef struct tkey_t *tkey_handle_t;
+
+typedef void (*tkey_event_cb_t)(tkey_handle_t key, tkey_event_t event,
+                                uint8_t press_count, void *user_data);
+typedef int (*tkey_detect_cb_t)(void);
+
+typedef struct
+{
+    tkey_event_cb_t event_cb;
+    tkey_detect_cb_t detect_cb;
+    void *user_data;
+    uint16_t hold_ticks;
+    uint16_t debounce_ticks;
+    uint16_t multi_press_interval_ticks;
+    uint16_t pressed_level;
+} tkey_init_t;
+
+tkey_handle_t tkey_create(tkey_init_t *init);
+void tkey_destroy(tkey_handle_t key);
+int tkey_check_init(tkey_handle_t key);
+void tkey_handler(tkey_handle_t key);
+void tkey_register_cbs(tkey_handle_t key, tkey_event_cb_t event_cb,
+                        tkey_detect_cb_t detect_cb, void *user_data);
+void tkey_set_pressed_level(tkey_handle_t key, uint8_t pressed_level);
+tkey_handle_t tkey_create_default(void); // based on a polling frequency of 100Hz
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
